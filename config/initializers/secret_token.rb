3c78725da7825f3e95d14d8ac0766688ea26782e
Mark Lehman & Liz Tom <student@epicodus.com>
2014-04-07 09:05:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BusTimetable::Application.config.secret_key_base = 'fe852a4cf4de3b89284ed04c4d09e2023d3928e5711e3bc5e29b67294f6e66e9cae73d29a4e86bc5b111a5c0618651b65b9b21fe67c4a651b0adcf3cde3bc831'
